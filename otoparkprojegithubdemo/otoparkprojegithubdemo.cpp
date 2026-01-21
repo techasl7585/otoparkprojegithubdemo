3c90700a6753ddf55e88197b6ee979fde861ec99
Mahmut Asel Akyürek <aky.asl@outlook.com>
2026-01-21 22:34:53 +0300

    başlangıc yapıldı

--- otoparkprojegithubdemo/otoparkprojegithubdemo.cpp
+++ otoparkprojegithubdemo/otoparkprojegithubdemo.cpp
@@ -1,24 +1,88 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS 
+// DİKKAT VİSUAL STUDİO HARİCİ KULLANIYORSANIZ KODU SİLİN #define _CRT_SECURE_NO_WARNINGS 
+
+
 #include <stdio.h>
 #include <iostream>
 
 int main()
 {
-	printf("otopark proje github demo\n");
-	printf("sayi giriniz: ");
-	int sayi;
-	scanf("%d", &sayi);
-	printf("girdiginiz sayi: %d\n", sayi);
+	printf("\n\n");
+	printf("Otopark sistemine hosgeldiniz\n");
+	printf("\n\n");
+
+
+	printf("otoparkin kapasitesini belirleyiniz:\n");
+	int kap;
+	scanf("%d", &kap);
 
 
 
 
-	//github değisikligi denemesi
 
-	printf("degisiklik 1\n");
+
+	printf("1-Araba Girisi\n");
+	printf("2-Araba cikisi\n");
+	printf("3-Otopark Durumu\n");
+	printf("4-Arac Sorgulama\n");
+	printf("5-Gunluk Kazanc Hesaplama\n");
+	printf("6-Kurallara Gore Raporlama\n");
+	printf("7-Programdan cikis\n");
+
+	int secim;
+	scanf("%d", &secim);
+
 	
-	printf("degisiklik 2\n");
-	
+		switch (secim)
+	{
+	case 1:
+		printf("Araba Girisi Seçildi\n");
+		printf("\n\n");
+		printf("plaka girin bosluk kullanmayin:\n");
+		char plaka[10];
+		scanf("%s", &plaka);
+		printf("Girilen plaka: %s\n", plaka);
+
+
+
+
+
+		break;
+	case 2:
+		printf("Araba cikisi Seçildi\n");
+		printf("\n\n");
+		printf("cikarmak istediginiz plakayi secin\n");
+		printf("%s\n", plaka);
+
+
+
+
+
+
+		break;
+	case 3:
+		printf("Otopark Durumu Seçildi\n");
+		break;
+	case 4:
+		printf("Araç Sorgulama Seçildi\n");
+		break;
+	case 5:
+		printf("Günlük Kazanç Hesaplama Seçildi\n");
+		break;
+	case 6:
+		printf("Kurallara Göre Raporlama Seçildi\n");
+		break;
+	case 7:
+		printf("Programdan Çıkış Seçildi\n");
+		break;
+	}
+
+
+
+
+
+
+
 
 
 
@@ -28,14 +92,3 @@ int main()
 
 
 }
-
-// Programı çalıştır: Ctrl + F5 veya Hata Ayıkla > Hata Ayıklamadan Başlat menüsü
-// Programda hata ayıkla: F5 veya Hata Ayıkla > Hata Ayıklamayı Başlat menüsü
-
-// Kullanmaya Başlama İpuçları: 
-//   1. Dosyaları eklemek/yönetmek için Çözüm Gezgini penceresini kullanın
-//   2. Kaynak denetimine bağlanmak için Takım Gezgini penceresini kullanın
-//   3. Derleme çıktısını ve diğer iletileri görmek için Çıktı penceresini kullanın
-//   4. Hataları görüntülemek için Hata Listesi penceresini kullanın
-//   5. Yeni kod dosyaları oluşturmak için Projeye Git > Yeni Öğe ekle veya varolan kod dosyalarını projeye eklemek için Proje > Var Olan Öğeyi Ekle adımlarını izleyin
-//   6. Bu projeyi daha sonra yeniden açmak için Dosya > Aç > Proje'ye gidip .sln uzantılı dosyayı seçin
