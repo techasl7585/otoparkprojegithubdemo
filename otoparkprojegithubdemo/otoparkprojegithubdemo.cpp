1bce6480b2e4c102438b2c6c0a65dd2d46002469
Mahmut Asel Akyürek <aky.asl@outlook.com>
2026-01-27 13:12:38 +0300

    kapasiteden fazla araç girdiğinde araç girişini kısıtlaması eklendi

--- otoparkprojegithubdemo/otoparkprojegithubdemo.cpp
+++ otoparkprojegithubdemo/otoparkprojegithubdemo.cpp
@@ -1,94 +1,59 @@
-﻿#define _CRT_SECURE_NO_WARNINGS 
-// DİKKAT VİSUAL STUDİO HARİCİ KULLANIYORSANIZ KODU SİLİN #define _CRT_SECURE_NO_WARNINGS 
-
-
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
-#include <iostream>
 
-int main()
-{
-	printf("\n\n");
-	printf("Otopark sistemine hosgeldiniz\n");
-	printf("\n\n");
-
-
-	printf("otoparkin kapasitesini belirleyiniz:\n");
-	int kap;
-	scanf("%d", &kap);
+int main() {
+    int kap;
+    int aracSayisi = 0;
+    int secim;
+    char plaka[10];
 
 
+    printf("\n\nOtopark sistemine hosgeldiniz\n\n");
+    printf("Otoparkin kapasitesini belirleyiniz:\n");
+    scanf("%d", &kap);
 
+    do {
+        printf("1-Araba Girisi\n");
+        printf("2-Araba cikisi\n");
+        printf("3-Otopark Durumu\n");
+        printf("4-Arac Sorgulama\n");
+        printf("5-Gunluk Kazanc Hesaplama\n");
+        printf("6-Kurallara Gore Raporlama\n");
+        printf("0-Programdan cikis\n");
 
+        scanf("%d", &secim);
 
+        switch (secim) {
+        case 1:
+            if (aracSayisi >= kap) {
+                printf("Otopark dolu!\n");
+                break;
+            }
+            printf("Araba Girisi Seçildi\n");
+            printf("Plaka girin (bosluk kullanmayin): ");
+            scanf("%s", plaka);
+            printf("Girilen plaka: %s\n", plaka);
+            aracSayisi++;
+            break;
 
-	printf("1-Araba Girisi\n");
-	printf("2-Araba cikisi\n");
-	printf("3-Otopark Durumu\n");
-	printf("4-Arac Sorgulama\n");
-	printf("5-Gunluk Kazanc Hesaplama\n");
-	printf("6-Kurallara Gore Raporlama\n");
-	printf("7-Programdan cikis\n");
+        case 2:
+            printf("Araba cikisi Seçildi\n");
+            printf("Cikarmak istediginiz plaka: %s\n", plaka);
+            if (aracSayisi > 0) aracSayisi--;
+            break;
 
-	int secim;
-	scanf("%d", &secim);
+        case 3:
+            printf("Otopark Durumu: %d/%d dolu\n", aracSayisi, kap);
+            break;
 
-	
-		switch (secim)
-	{
-	case 1:
-		printf("Araba Girisi Seçildi\n");
-		printf("\n\n");
-		printf("plaka girin bosluk kullanmayin:\n");
-		char plaka[10];
-		scanf("%s", &plaka);
-		printf("Girilen plaka: %s\n", plaka);
+        case 0:
+            printf("Programdan cikiliyor...\n");
+            break;
 
+        default:
+            printf("Hatali secim!\n");
+        }
+    } while (secim != 0);
 
-
-
-
-		break;
-	case 2:
-		printf("Araba cikisi Seçildi\n");
-		printf("\n\n");
-		printf("cikarmak istediginiz plakayi secin\n");
-		printf("%s\n", plaka);
-
-
-
-
-
-
-		break;
-	case 3:
-		printf("Otopark Durumu Seçildi\n");
-		break;
-	case 4:
-		printf("Araç Sorgulama Seçildi\n");
-		break;
-	case 5:
-		printf("Günlük Kazanç Hesaplama Seçildi\n");
-		break;
-	case 6:
-		printf("Kurallara Göre Raporlama Seçildi\n");
-		break;
-	case 7:
-		printf("Programdan Çıkış Seçildi\n");
-		break;
-	}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
 }
