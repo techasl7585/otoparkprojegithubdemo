fc1614edf2845a0602ca00cbcd3c6dbbf72f404c
Mahmut Asel Akyürek <aky.asl@outlook.com>
2026-01-28 18:14:54 +0300

    araç çıkış işlemleri ve süre ve ücret işlemleri tamamlandı ücretler readme dosyasında belirtildi

--- otoparkprojegithubdemo/otoparkprojegithubdemo.cpp
+++ otoparkprojegithubdemo/otoparkprojegithubdemo.cpp
@@ -7,6 +7,7 @@ int main() {
     int kap;
     int aracSayisi = 0;
     int secim;
+    int toplamKazanc = 0;
 
     char plaka[10];
     char otopark[100][10];    // Hafıza oluşturduk aynı plakalı aracı iki sefer alamamak için
@@ -18,7 +19,7 @@ int main() {
     scanf("%d", &kap);
 
     do {
-        printf("1-Araba Girisi\n");
+        printf("\n\n1-Araba Girisi\n");
         printf("2-Araba cikisi\n");
         printf("3-Otopark Durumu\n");
         printf("4-Arac Sorgulama\n");
@@ -34,7 +35,7 @@ int main() {
                 printf("Otopark dolu!\n");
                 break;
             }
-            printf("Araba Girisi Secildi\n");
+            printf("\n\nAraba Girisi Secildi\n");
             printf("Plaka girin (bosluk kullanmayin): ");
             scanf("%s", plaka);
 
@@ -71,13 +72,64 @@ int main() {
             break;
         }
 
-        case 2:
-            printf("Araba cikisi Secildi\n");
+        case 2:{
+            printf("\n\nAraba cikisi Secildi\n");
             printf("Cikarmak istediginiz plakayi girin (bosluk kullanmayin): ");
-            scanf("%s", plaka);
-            printf("cikarilan plaka: %s\n", plaka);
-            aracSayisi--;
-            break;
+            scanf("%9s", plaka);
+
+
+			int bulundu = -1;
+            for (int i = 0; i < aracSayisi; i++) {
+                if (strcmp(otopark[i], plaka) == 0) {
+                    bulundu = i; break;
+                }
+            }
+            if (bulundu == -1) {
+                printf("Arac bulunamadi!\n");
+            }
+                  
+            else {
+				int cikissaat = 0;
+				int cikisdakika = 0;
+				int ucret = 0;
+				printf("Arac cikis saati (0-23) giriniz: ");
+				scanf("%d", &cikissaat);
+				printf("Arac cikis dakikasi (0-59) giriniz: ");
+				scanf("%d", &cikisdakika);
+
+                // Toplam kalinan saati hesapla (Basit mantik)
+                int farkDakika = (cikissaat * 60 + cikisdakika) - (girisSaatleri[bulundu] * 60 + girisDakikalari[bulundu]);
+                if (farkDakika < 0) farkDakika += 24 * 60; // Gece yarisini gectiyse
+
+                int toplamSaat = farkDakika / 60;
+                if (farkDakika % 60 > 0) toplamSaat++; // 1 dakika bile gecse tam saat sayilir
+
+                // Tarifeyi belirle (Giris saatine gore)
+                int gSaat = girisSaatleri[bulundu];
+                if (gSaat >= 6 && gSaat < 22) { // GUNDUZ TARIFESI
+                    if (toplamSaat <= 4) ucret = 50 + (toplamSaat - 1) * 20;
+                    else ucret = 110 + (toplamSaat - 4) * 40;
+                    printf("Gunduz tarifesi uygulandi.\n");
+                }
+                else { // GECE TARIFESI
+                    if (toplamSaat <= 4) ucret = toplamSaat * 100;
+                    else ucret = 400 + (toplamSaat - 4) * 150;
+                    printf("Gece tarifesi uygulandi.\n");
+                }
+
+                printf("Sure: %d saat | Borcunuz: %d TL\n", toplamSaat, ucret);
+                toplamKazanc += ucret; // Kazancı ekle
+
+                // Aracı diziden silme (Kaydirma islemi)
+                for (int j = bulundu; j < aracSayisi - 1; j++) {
+                    strcpy(otopark[j], otopark[j + 1]);
+                    girisSaatleri[j] = girisSaatleri[j + 1];
+                    girisDakikalari[j] = girisDakikalari[j + 1];
+                }
+                aracSayisi--;
+                }
+                break;
+        }
 
         case 3:
             printf("Otopark Durumu: %d/%d dolu\n", aracSayisi, kap);
